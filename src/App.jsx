3dfce6b23245a8f195ea9c463fe1638545899002
Yuliia Garreu <yuliia.garreau@gmail.com>
2025-02-22 00:36:20 +0000

    save

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,15 @@ import SearchBox from "./components/SearchBox/SearchBox";
 import ContactList from "./components/ContactList/ContactList";
 import "./App.css";
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { fetchContacts } from "./redux/contactsOps";
 
 function App() {
   const dispatch = useDispatch();
-
+  const { items, isLoading, error } = useSelector((state) => state.contacts);
   useEffect(() => {
     dispatch(fetchContacts());
-  });
+  }, [dispatch]);
   return (
     <div>
       <h1>Phonebook</h1>
